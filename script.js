0b0fca21acdf40a76a3900498e182a268326e14d
Jake Ruiz <jwr318@gmail.com>
2022-02-14 16:28:11 -0500

    created the game function which plays 5 rounds and returns the winner as an alert

--- script.js
+++ script.js
@@ -1,73 +1,126 @@
-function computerPlay(){
+function computerPlay() {
     let computerChoices = ["rock", "paper", "scissor"];
-    let randomNum = Math.floor(Math.random() * (2 - 0 + 1) + 0); 
+    let randomNum = Math.floor(Math.random() * (2 - 0 + 1) + 0);
     return computerChoices[randomNum];
-  
+
 }
 
 
-function playerPlay(){
-    let getInput = prompt("Select either Rock, Paper or Scissors: ").toLowerCase(); 
+function playerPlay() {
+    let getInput = prompt("Select either Rock, Paper or Scissors: ").toLowerCase();
     let playerSelection = getInput;  // this prompt is running twice, need to find the fix  
-    if(playerSelection  == "rock" || playerSelection == "paper" || playerSelection == "scissors"){
-     return playerSelection;
+    if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors") {
+        return playerSelection;
     }
 
     else {
-    playerPlay();
+        playerPlay();
     }
 }
 
 
-function roundPlay(){
+function roundPlay() {
     let c = computerPlay();
     let p = playerPlay();
     let lose = `You Lose! ${c} beats ${p}`;
     let win = `You Win! ${p} beats ${c}`;
     let tie = "You tied!";
- 
-    
-  switch(p) {
+    let result = "";
 
-    case "rock":
-        if(c == "paper"){
-            alert(lose);
-        }
-        else if(c == "scissors"){
-            alert(win);
-        }
-        else {
-            alert(tie)
-        }
-        break;
+    switch (p) {
 
-    case "paper":
-        if(c == "scissors"){
-            alert(lose);
-        }
-        else if(c == "rock"){
-            alert(win);
-        }
-        else {
-            alert(tie)
-        }
-        break;
+        case "rock":
+            if (c == "paper") {
+                alert(lose);
+                result = "computer";
+            }
+            else if (c == "scissors") {
+                alert(win);
+                result = "player";
+            }
+            else {
+                alert(tie)
+                result = "tie";
+            }
+            break;
 
-    case "scissors":
-        if(c == "rock"){
-            alert(lose);
-        }
-        else if(c == "paper"){
-            alert(win);
-        }
-        else {
-            alert(tie)
-        }
-        break;
+        case "paper":
+            if (c == "scissors") {
+                alert(lose);
+                result = "computer";
+            }
+            else if (c == "rock") {
+                alert(win);
+                result = "player";
+            }
+            else {
+                alert(tie)
+                result = "tie";
+            }
+            break;
 
-    default :
-        alert("Something went wrong");
-  }
+        case "scissors":
+            if (c == "rock") {
+                alert(lose);
+                result = "computer";
+            }
+            else if (c == "paper") {
+                alert(win);
+                result = "player";
+            }
+            else {
+                alert(tie)
+                result = "tie";
+            }
+            break;
+
+        default:
+            alert("Something went wrong");
+    }
+    return result;
 }
 
-roundPlay();
+
+function game() {
+    // have a variable that stores computer wins
+    let cScore = 0;
+    // have a variable that stores player wins
+    let pScore = 0;
+    // have a variable that stores ties
+    let tScore = 0;
+    // loop for 5 games 
+    while (cScore + pScore + tScore < 5) {
+        let winner = roundPlay();
+        switch (winner) {
+            case "player":
+                pScore += 1;
+                break;
+
+            case "computer":
+                cScore += 1;
+                break;
+
+            default:
+                tScore += 1;
+                break;
+
+        }
+        console.log(`the current score is Computer: ${cScore} versus Player: ${pScore}. There have been ${tScore} ties`)
+    }
+
+    if (pScore > cScore) {
+        alert("Player wins! Computer loses!")
+    }
+    else if (pScore < cScore) {
+        alert("Computer wins! Player loses!")
+    }
+
+    else {
+        alert("It was a tie");
+    }
+    return;
+
+}
+
+game();
+//roundPlay();
